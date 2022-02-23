22c6a5b5f8215eea714d446cc8ba0b1bc2728d21
unknown <khongtiennam@gmail.com>
2022-02-23 16:24:49 +0700

    Create_Project_BaiThucHanh2202

---
+++ BaiThucHanh2302/BaiThucHanh2202/src/App5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class App5 {
+    public static void main(String[] args) {
+        Scanner m = new Scanner(System.in);
+    int sum = 0, n;
+    System.out.println("\n\nNhap vao so can kiem tra: ");
+    n = m.nextInt();
+    for(int i=1;i<=n/2;i++){
+      if(n%i==0) 
+        sum+=i;
+    }
+    if(sum==n){
+      System.out.println(n + " la so hoan thien");
+    }
+    else {
+      System.out.println(n + " khong phai là so hoan thien");
+    }
+    }
+}
+
---
+++ BaiThucHanh2302/BaiThucHanh2202/src/App9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class App9 {
+    public static void main(String[] args) {
+        Scanner m = new Scanner(System.in);
+        int num1,num2, reversed = 0;
+        System.out.println("Nhap vao so can đao nguoc: ");
+        num1 = m.nextInt();
+        num2 = num1;
+        while(num1 != 0) {
+          int digit = num1 % 10;
+          reversed = reversed * 10 + digit;
+          num1 /= 10;
+        }
+        System.out.printf("So "+ num2 +" sau khi đao nguoc la: " + reversed);
+      }
+    
+}
